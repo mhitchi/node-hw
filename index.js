e94619bd1f73dbc6a7fe26837591f03699874b27
mhitchi <mayhitchings@gmail.com>
2020-01-22 18:31:39 -0500

    calling axios and console.logging info except starred

--- index.js
+++ index.js
@@ -32,20 +32,44 @@ const questions = [
 // use inquirer to prompt questions
 inquirer.prompt(questions).then(function(response) {
   console.log(response);
+
+  var data = {
+    username: response.username,
+    color: response.color
+  }
+  getGithubInfo(data.username);
 });
 
+function getGithubInfo(user) {
+  axios.get("https://api.github.com/users/" + user).then(function(response) {
+    //image
+    console.log(response.data.avatar_url);
+    //username
+    console.log(user);
+    //link to location via google maps
+    console.log(response.data.location);
+    //link to github profile
+    console.log(response.data.html_url);
+    //link to blog
+    console.log(response.data.blog);
+    //bio
+    console.log(response.data.bio);
+    //number of repos
+    console.log(response.data.public_repos);
+    //number of followers
+    console.log(response.data.followers);
+    //number of Github stars
+    //TODO returning undefined
+    console.log(response.data.starred);
+    //number following
+    console.log(response.data.following);
+  }).catch(function(err) {
+    console.log(err);
+  });
+};
+
 // function writeToFile(fileName, data) {
-//   // * Profile image
-//   // * User name
-//   // * Links to the following:
-//   //   * User location via Google Maps
-//   //   * User GitHub profile
-//   //   * User blog
-//   // * User bio
-//   // * Number of public repositories
-//   // * Number of followers
-//   // * Number of GitHub stars
-//   // * Number of users following
+
  
 // }
 
